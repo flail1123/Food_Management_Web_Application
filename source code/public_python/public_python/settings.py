4d83a9a02770c8ddfe151748653c96fbbd655bf3
flail1123 <59021655+flail1123@users.noreply.github.com>
2021-02-12 21:10:55 +0100

    Update settings.py

--- source code/public_python/public_python/settings.py
+++ source code/public_python/public_python/settings.py
@@ -72,16 +72,11 @@ WSGI_APPLICATION = 'public_python.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = { #dane usunięte dla bezpieczeństwa
+DATABASES = { #data removed for safety
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
