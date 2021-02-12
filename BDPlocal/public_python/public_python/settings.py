8a158a8d820bd06274588eecd37bebd74f108734
flail1123 <59021655+flail1123@users.noreply.github.com>
2021-02-12 22:01:10 +0100

    Update settings.py

--- BDPlocal/public_python/public_python/settings.py
+++ BDPlocal/public_python/public_python/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '12345678'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bdproj.kzkaktus.usermd.net', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
